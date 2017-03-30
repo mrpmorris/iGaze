bd646ee8b4d4d1023af5ada3c1ebf12ebe5d3b20
Peter Morris <mrpmorris@gmail.com>
2017-03-30 11:09:03 +0100

    Gaze classes

---
+++ iGaze/GazeSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+
+namespace iGaze
+{
+	public abstract class GazeSource : IDisposable
+	{
+		public Point DataPoint { get; private set; }
+		public DateTime DataTimeStamp { get; private set; }
+		public bool IsDisposed { get; private set; }
+
+		private KalmanFilter KalmanX;
+		private KalmanFilter KalmanY;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!IsDisposed)
+			{
+				if (disposing)
+				{
+				}
+				IsDisposed = true;
+			}
+		}
+
+		public virtual void Dispose()
+		{
+			Dispose(true);
+		}
+
+		protected void OnGazeDataReceived(double x, double y)
+		{
+			ApplyKalmanFilters(ref x, ref y);
+			DataTimeStamp = DateTime.UtcNow;
+			DataPoint = new Point((int)x, (int)y);
+			System.Windows.Forms.Cursor.Position = DataPoint;
+		}
+
+		private void ApplyKalmanFilters(ref double x, ref double y)
+		{
+			if (KalmanX == null)
+			{
+				KalmanX = new KalmanFilter(x, 0.5, 1.5, 1000);
+				KalmanY = new KalmanFilter(y, 0.5, 1.5, 1000);
+			}
+			x = KalmanX.Update(x);
+			y = KalmanY.Update(y);
+		}
+	}
+}
---
+++ iGaze/GazeSources/Tobii/TobiiGazeSource.cs
@@ -0,0 +1,36 @@
+﻿using EyeXFramework;
+using Tobii.EyeX.Framework;
+
+namespace iGaze.GazeSources.Tobii
+{
+	public class TobiiGazeSource : GazeSource
+	{
+		private double LastTimeStamp = -1;
+		private readonly EyeXHost EyeXHost;
+		private readonly GazePointDataStream GazePointDataStream;
+
+		public TobiiGazeSource()
+		{
+			EyeXHost = new EyeXHost();
+			GazePointDataStream = EyeXHost.CreateGazePointDataStream(GazePointDataMode.Unfiltered);
+			GazePointDataStream.Next += GazeData;
+			EyeXHost.Start();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+				EyeXHost.Dispose();
+			base.Dispose(disposing);
+		}
+
+		private void GazeData(object sender, GazePointEventArgs e)
+		{
+			if (e.Timestamp != LastTimeStamp)
+			{
+				LastTimeStamp = e.Timestamp;
+				OnGazeDataReceived(e.X, e.Y);
+			}
+		}
+	}
+}
