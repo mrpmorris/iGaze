a3182fa7a182cc9716189fed1f11c8666decc069
Peter Morris <mrpmorris@gmail.com>
2017-03-31 09:24:56 +0100

    Keyboard click sound

--- iGaze/MainWindow.xaml.cs
+++ iGaze/MainWindow.xaml.cs
@@ -59,7 +59,11 @@ namespace iGaze
 		private void Timer_Tick(object sender, EventArgs e)
 		{
 			if (DateTime.UtcNow - GazeSource.DataTimeStamp < GazeTimeMilliseconds)
+			{
+				KeyHitAudio.Position = TimeSpan.Zero;
+				KeyHitAudio.Play();
 				TimerAction?.Invoke();
+			}
 		}
 
 
